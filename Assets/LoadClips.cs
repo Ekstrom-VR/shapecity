1c99e6d5c327a9001117afb71cbd66663087a315
Jared Stokes <stokesjd@gmail.com>
2018-04-03 16:39:42 -0700

    added new load video.cs method

---
+++ Assets/LoadClips.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class LoadClips : MonoBehaviour {
+
+	void Start () {
+		string path = "NavClipsFinalRand/";
+//		List<string> clipPaths = new List<string> ();
+//		DirectoryInfo info = new DirectoryInfo(path);
+		foreach(string file in System.IO.Directory.GetFiles(path)) 
+		{ 
+			print (file);
+		}
+	}
